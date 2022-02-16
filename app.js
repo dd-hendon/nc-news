f2c33e014d09c144c9f3c97885fca4a3931b8b96
dd-hendon <d.hendon17@gmail.com>
2022-02-16 12:33:52 +0000

    Complete ticket 7

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
-const { getArticleById } = require("./controllers/articles-controller");
+const {
+  getArticleById,
+  patchArticleById,
+} = require("./controllers/articles-controller");
 const { getTopics } = require("./controllers/topics-controller");
 const {
   handleServerErrors,
@@ -12,6 +15,7 @@ app.use(express.json());
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticleById);
+app.patch("/api/articles/:article_id", patchArticleById);
 
 app.all("/*", (req, res) => {
   res.status(404).send({ message: "Path not found" });
