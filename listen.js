af5897f2f3b3e02827216f52a7363e01a2d90339
dd-hendon <d.hendon17@gmail.com>
2022-02-19 16:00:08 +0000

    PORT change

--- listen.js
+++ listen.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 
-const { PORT = 9090 } = process.env;
+const PORT = process.env.PORT || 9090;
+console.log(PORT);
 
 app.listen(PORT, (err) => {
   if (err) throw err;
